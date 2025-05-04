19893b15e6cf3d64b414cf502d976e5dd6d0120d
KiramZodiac <kiramzodiac@gmail.com>
2025-05-04 15:08:24 +0300

    added server configs

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -60,7 +60,7 @@ const Booking = () => {
   const onSubmit = async (formData) => {
     setIsSubmitting(true)
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch('https://nails-website.onrender.com/send-email', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
